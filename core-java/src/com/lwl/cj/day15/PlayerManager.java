7f3761528776b0b6fc34ad6177cdf5ed91f705ea
lwl <learnwithlakshman@gmail.com>
2022-03-11 07:08:42 +0530

    Added list example

--- core-java/src/com/lwl/cj/day15/PlayerManager.java
+++ core-java/src/com/lwl/cj/day15/PlayerManager.java
@@ -6,7 +6,21 @@ public class PlayerManager {
 			
 				PlayerService pservice = new PlayerServiceImpl();
 				
+			
 				int count = pservice.playerCount((p)->p.getRole().equals("Bowler") && p.getPrice()==2000000);
+				
+				
+				 // Find count of players for the given team
+			
+				// Find count of players for the given team and role
+
+				// Find sum of amount by the role
+				
+				// Find sum of amount for the  given team
+				
+				// Find sum of amount for the given role and team
+				
+				
 				System.out.println(count);
 		}
 }
---
+++ core-java/src/com/lwl/cj/day16/ListAssignment.java
@@ -0,0 +1,32 @@
+package com.lwl.cj.day16;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ListAssignment {
+
+	public boolean isSubSet(List<Integer> list1, List<Integer> list2) {
+		return false;
+	}
+	
+	public List<Integer> getElements(List<Integer> list,Predicate<Integer> p){
+		
+		return null;
+	}
+	
+	public List<Integer> getUniqueElements(List<Integer> list){
+		return null;
+	}
+	public List<Integer> getCombList(List<Integer> list1,List<Integer> list2){
+		return null;
+	}
+	
+	public boolean searchElement(List<Integer> list,Integer key) {
+		return false;
+	}
+	// Desc | ASC 
+	public List<Integer> sort(List<Integer> list,String order){
+		
+		return null;
+	}
+}
